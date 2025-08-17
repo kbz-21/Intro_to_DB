b22feb7377f97ab0e112e9aee33c4aa1ca775a13
Kaleab Zewdie <kalzed1921@gmail.com>
2025-08-17 22:39:04 +0300

    Add Task 5: insert first customer row

---
+++ task_5.sql
@@ -0,0 +1,9 @@
+-- =========================================================
+-- Task 5: Insert single customer row
+-- File: task_5.sql
+-- =========================================================
+
+USE alx_book_store;
+
+INSERT INTO Customers (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
