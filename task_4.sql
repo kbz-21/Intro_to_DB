c89871145811d8c928d5e164612e932ba19bdce8
Kaleab Zewdie <kalzed1921@gmail.com>
2025-08-17 22:36:48 +0300

    Add Task 4: full description of Books table

---
+++ task_4.sql
@@ -0,0 +1,18 @@
+-- =========================================================
+-- Task 4: Full description of 'books' table
+-- File: task_4.sql
+-- =========================================================
+
+USE alx_book_store;
+
+SELECT 
+    COLUMN_NAME AS 'Column',
+    COLUMN_TYPE AS 'Type',
+    IS_NULLABLE AS 'Nullable',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'Books'
+ORDER BY ORDINAL_POSITION;
